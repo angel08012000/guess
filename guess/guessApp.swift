e4d7bca4ce4bf9b9e8285295d5f5e4544b05d47a
Cih <angel08012000@gmail.com>
2021-03-03 14:41:01 +0800

    Initial Commit

---
+++ guess/guessApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessApp.swift
+//  guess
+//
+//  Created by 慈慈 on 2021/3/3.
+//
+
+import SwiftUI
+
+@main
+struct guessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
